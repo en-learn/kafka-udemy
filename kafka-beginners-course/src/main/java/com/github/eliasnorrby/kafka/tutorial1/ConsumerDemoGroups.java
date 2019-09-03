613bea395114bdc0e8f8fdf61447df46f927dfef
Elias Norrby <elias.norrby@gmail.com>
2019-09-03 07:46:05 +0200

    Switch to 2 space indent, reformat all files

--- kafka-beginners-course/src/main/java/com/github/eliasnorrby/kafka/tutorial1/ConsumerDemoGroups.java
+++ kafka-beginners-course/src/main/java/com/github/eliasnorrby/kafka/tutorial1/ConsumerDemoGroups.java
@@ -1,5 +1,8 @@
 package com.github.eliasnorrby.kafka.tutorial1;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -8,44 +11,41 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Properties;
-
 public class ConsumerDemoGroups {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
+    Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class.getName());
 
-        String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "my-fifth-application";
-        String topic = "first_topic";
+    String bootstrapServers = "127.0.0.1:9092";
+    String groupId = "my-fifth-application";
+    String topic = "first_topic";
 
-        // create consumer configs
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    // create consumer configs
+    Properties properties = new Properties();
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    properties.setProperty(
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        // create consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+    // create consumer
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
-        // subscribe consumer to our topic(s)
-        consumer.subscribe(Arrays.asList(topic));
+    // subscribe consumer to our topic(s)
+    consumer.subscribe(Arrays.asList(topic));
 
-        // poll for new data
-        while (true) {
-//            consumer.poll(100); // new in Kafka 2.0.0
-            ConsumerRecords<String, String> records =
-                consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+    // poll for new data
+    while (true) {
+      //            consumer.poll(100); // new in Kafka 2.0.0
+      ConsumerRecords<String, String> records =
+          consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
 
-            for (ConsumerRecord<String, String> record : records) {
-                logger.info("Key: " + record.key() + ", Value: " + record.value());
-                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
-            }
-
-        }
+      for (ConsumerRecord<String, String> record : records) {
+        logger.info("Key: " + record.key() + ", Value: " + record.value());
+        logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+      }
     }
+  }
 }
--- kafka-beginners-course/src/main/java/com/github/eliasnorrby/kafka/tutorial1/ProducerDemoWithCallback.java
+++ kafka-beginners-course/src/main/java/com/github/eliasnorrby/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -1,55 +1,67 @@
 package com.github.eliasnorrby.kafka.tutorial1;
 
+import java.util.Properties;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Properties;
-
 public class ProducerDemoWithCallback {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+    final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
 
-        // create Producer properties
-        Properties properties = new Properties();
-        String bootstrapServers = "127.0.0.1:9092";
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    // create Producer properties
+    Properties properties = new Properties();
+    String bootstrapServers = "127.0.0.1:9092";
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    properties.setProperty(
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    properties.setProperty(
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // create the producer
-        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+    // create the producer
+    final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
 
-            // create a producer record
-            ProducerRecord<String, String> record =
-                    new ProducerRecord<String, String>("first_topic", "hello world " + Integer.toString(i));
+      // create a producer record
+      ProducerRecord<String, String> record =
+          new ProducerRecord<String, String>("first_topic", "hello world " + Integer.toString(i));
 
-            // send data -- asynchronous
-            producer.send(record, new Callback() {
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    // executes every time a record is successfully sent or an exception is thrown
-                    if (e == null) {
-                        // the record was successfully sent
-                        logger.info("Received new metadata. \n" +
-                                "Topic: " + recordMetadata.topic() + "\n" +
-                                "Partition: " + recordMetadata.partition() + "\n" +
-                                "Offset: " + recordMetadata.offset() + "\n" +
-                                "Timestamp: " + recordMetadata.timestamp() + "\n");
-                    } else {
-                        logger.error("Error while producing", e);
-                    }
-                }
-            });
-        }
-
-        // flush data
-        producer.flush();
-        // flush and close
-        producer.close();
+      // send data -- asynchronous
+      producer.send(
+          record,
+          new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+              // executes every time a record is successfully sent or an exception is thrown
+              if (e == null) {
+                // the record was successfully sent
+                logger.info(
+                    "Received new metadata. \n"
+                        + "Topic: "
+                        + recordMetadata.topic()
+                        + "\n"
+                        + "Partition: "
+                        + recordMetadata.partition()
+                        + "\n"
+                        + "Offset: "
+                        + recordMetadata.offset()
+                        + "\n"
+                        + "Timestamp: "
+                        + recordMetadata.timestamp()
+                        + "\n");
+              } else {
+                logger.error("Error while producing", e);
+              }
+            }
+          });
     }
+
+    // flush data
+    producer.flush();
+    // flush and close
+    producer.close();
+  }
 }
